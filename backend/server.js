b3cd7ab9a46b302e2832a3df8e0fe19d916d5a99
wx-yz <57770159+wx-yz@users.noreply.github.com>
2025-08-08 23:11:18 -0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
